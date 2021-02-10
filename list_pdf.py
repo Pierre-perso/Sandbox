914158856b7337c1e42dc0c6994c281bde2825b9
Pierre-perso <78846058+Pierre-perso@users.noreply.github.com>
2021-02-10 09:43:01 +0100

    Add files via upload

---
+++ list_pdf.py
@@ -0,0 +1,6 @@
+import os
+
+all_files = []
+for dirpath, dirnames, filenames in os.walk("."):
+    for filename in [f for f in filenames if f.endswith(".pdf")]:
+        all_files.append(os.path.join(dirpath, filename)
